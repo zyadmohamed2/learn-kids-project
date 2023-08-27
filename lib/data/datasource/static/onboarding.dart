f3f2288b85887669c681b0c6e57f9eec5cfbb7d6
zyad-dark <zyadmohamed122232@gmail.com>
2023-08-27 13:50:19 +0300

    zyad

---
+++ lib/data/datasource/static/onboarding.dart
@@ -0,0 +1,22 @@
+import 'package:learn/data/model/onboarding_model.dart';
+
+List<OnBoardingModel> onBoardingList = [
+  OnBoardingModel(
+    "assets/svg/supimage1.svg", 
+    "assets/gif/animation_lltaiylp_small.gif", 
+    "assets/svg/onboarding_text1.svg",
+    "assets/svg/background.svg"
+  ),
+  OnBoardingModel(
+    "assets/svg/supimage2.svg", 
+    "assets/gif/animation_lltakso0_small.gif", 
+    "assets/svg/onboarding_text2.svg",
+    "assets/svg/background.svg"
+  ),
+  OnBoardingModel(
+    "assets/svg/supimage3.svg", 
+    "assets/gif/animation_lltajg5v_small.gif", 
+    "assets/svg/onboarding_text3.svg",
+    "assets/svg/background_onboarding.svg"
+  ),
+];
---
+++ lib/data/model/onboarding_model.dart
@@ -0,0 +1,7 @@
+class OnBoardingModel{
+  final String supImage;
+  final String gif;
+  final String text;
+  final String background;
+  OnBoardingModel(this.supImage, this.gif, this.text, this.background);
+}
