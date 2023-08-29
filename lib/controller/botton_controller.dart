75a3fe20a2fe03d92eb36dfc714c1ae41c1ff929
zyad-dark <zyadmohamed122232@gmail.com>
2023-08-29 11:59:02 +0300

    zyad

---
+++ lib/controller/botton_controller.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:learn/data/datasource/static/onboarding.dart';
+import 'package:learn/services/services.dart';
+
+class BottonController extends GetxController{
+  MyServic myServic = Get.find();
+    
+}
